c7387b03a0a9c2622884028a6a057f526d877b4b
Kiyong Kim <scgyong@gmail.com>
2024-01-15 16:54:42 +0900

    Subview 활용하는 방법

--- Day1/ImageSwitcher/ImageSwitcher/ContentView.swift
+++ Day1/ImageSwitcher/ImageSwitcher/ContentView.swift
@@ -7,12 +7,23 @@
 
 import SwiftUI
 
-struct TopButtonModifier: ViewModifier {
-    func body(content: Content) -> some View {
-        return content
-            .background(Color.yellow)
-            .padding()
-            .background(Color.blue)
+
+struct TopButton: View {
+    var isLeft: Bool
+    var body: some View {
+        Button {
+            //page -= 1
+        } label: {
+            Image(systemName: 
+                isLeft ?
+                "arrow.left.circle.fill" :
+                "arrow.right.circle.fill"
+            )
+        }
+        .background(Color.yellow)
+        .padding()
+        .background(Color.blue)
+        //.disabled(page == 1)
     }
 }
 
@@ -22,23 +33,11 @@ struct ContentView: View {
     var body: some View {
         VStack {
             HStack {
-                Button {
-                    page -= 1
-                } label: {
-                    Image(systemName: "arrow.left.circle.fill")
-                }
-                .modifier(TopButtonModifier())
-                .disabled(page == 1)
+                TopButton(isLeft: true)
                 Text("\(page)/\(totalPage)")
                     //.background(Color.blue)
                     .frame(maxWidth: .infinity)
-                Button {
-                    page += 1
-                } label: {
-                    Image(systemName: "arrow.right.circle.fill")
-                }
-                .modifier(TopButtonModifier())
-                .disabled(page == totalPage)
+                TopButton(isLeft: false);
             }
             .font(.largeTitle)
             Image("cat\(page)")
@@ -52,3 +51,4 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
