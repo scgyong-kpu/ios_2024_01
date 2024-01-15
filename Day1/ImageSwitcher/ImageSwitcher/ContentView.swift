be35267625340d9623903f8fb3b4dd6fa0cf6072
Kiyong Kim <scgyong@gmail.com>
2024-01-15 15:09:07 +0900

    VStack and modifiers

--- Day1/ImageSwitcher/ImageSwitcher/ContentView.swift
+++ Day1/ImageSwitcher/ImageSwitcher/ContentView.swift
@@ -11,9 +11,23 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "arrow.up.bin")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+                //.font(.largeTitle)
+                .resizable()
+                .frame(width: 100, height: 100)
+                .foregroundColor(.accentColor)
             Text("Hello, SwiftUI !")
+                .font(.largeTitle)
+                .foregroundColor(Color/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                .background(Color.yellow)
+                .padding()
+                .border(Color.black)
+                .padding()
+                .background(Color.orange)
+            HStack {
+                Image(systemName: "pencil.circle.fill")
+                    .font(.largeTitle)
+                Text("Pencil")
+            }
         }
         .padding()
     }
