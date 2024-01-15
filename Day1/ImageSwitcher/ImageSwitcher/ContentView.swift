431b19c5bf05ff780a8208b9953edf38192f669d
Kiyong Kim <scgyong@gmail.com>
2024-01-15 14:21:23 +0900

    ImageSwitcher Skeleton

---
+++ Day1/ImageSwitcher/ImageSwitcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImageSwitcher
+//
+//  Created by Kiyong Kim on 1/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
