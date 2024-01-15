47fe004d3a066ac8afb3871de6d2f541e973ce68
Kiyong Kim <scgyong@gmail.com>
2024-01-15 15:29:55 +0900

    ImageSwitcher 기본화면구성

--- Day1/ImageSwitcher/ImageSwitcher/ContentView.swift
+++ Day1/ImageSwitcher/ImageSwitcher/ContentView.swift
@@ -10,36 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "arrow.up.bin")
-                //.font(.largeTitle)
-                .resizable()
-                //.frame(width: 100, height: 100)
-                .frame(maxWidth: .infinity)
-                .foregroundColor(.accentColor)
-                .background(
-                    Capsule()
-                        .foregroundColor(.yellow)
-                )
-            RoundedRectangle(cornerRadius: 50, style: .continuous)
-                .stroke()
-            Capsule()
-            Color.red
-            Text("Hello, SwiftUI !")
-                .font(.largeTitle)
-                .foregroundColor(Color/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                .background(Color.yellow)
-                .padding()
-                .border(Color.black)
-                .padding()
-                .background(Color.orange)
             HStack {
-                Image(systemName: "pencil.circle.fill")
-                Text("Pencil")
+                Image(systemName: "arrow.left.circle.fill")
+                Text("1/5")
+                Image(systemName: "arrow.right.circle.fill")
             }
-            .font(.largeTitle)
+            Image(systemName:"cat")
         }
         .padding()
-        .border(Color.blue)
     }
 }
 
