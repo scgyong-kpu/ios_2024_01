21ef6b52837791fa4eda6ff9c58caa5ae454ea8f
Kiyong Kim <scgyong@gmail.com>
2024-01-15 15:25:53 +0900

    RoundedRectangle, Capsule, Background

--- Day1/ImageSwitcher/ImageSwitcher/ContentView.swift
+++ Day1/ImageSwitcher/ImageSwitcher/ContentView.swift
@@ -13,8 +13,17 @@ struct ContentView: View {
             Image(systemName: "arrow.up.bin")
                 //.font(.largeTitle)
                 .resizable()
-                .frame(width: 100, height: 100)
+                //.frame(width: 100, height: 100)
+                .frame(maxWidth: .infinity)
                 .foregroundColor(.accentColor)
+                .background(
+                    Capsule()
+                        .foregroundColor(.yellow)
+                )
+            RoundedRectangle(cornerRadius: 50, style: .continuous)
+                .stroke()
+            Capsule()
+            Color.red
             Text("Hello, SwiftUI !")
                 .font(.largeTitle)
                 .foregroundColor(Color/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
