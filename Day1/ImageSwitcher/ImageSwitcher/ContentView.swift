501577ffb5f32cc3205d44b7195b8178156b3bc8
Kiyong Kim <scgyong@gmail.com>
2024-01-15 14:46:07 +0900

    Imgage 변경.

--- Day1/ImageSwitcher/ImageSwitcher/ContentView.swift
+++ Day1/ImageSwitcher/ImageSwitcher/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "arrow.up.bin")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, SwiftUI !")
         }
         .padding()
     }
