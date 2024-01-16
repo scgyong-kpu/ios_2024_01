08764fa6f3f84040df073b8c203bb13cd3199ec8
Kiyong Kim <scgyong@gmail.com>
2024-01-16 15:04:05 +0900

    MoreControls Skeleton

---
+++ Day2/MoreControls/MoreControls/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoreControls
+//
+//  Created by Kiyong Kim on 1/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
