fbbac4c4136024803579d341aa41a15aeba5a6d7
Kiyong Kim <scgyong@gmail.com>
2024-01-17 16:44:21 +0900

    Text 꾸미기

--- Day3/GestureSample/GestureSample/TapGestureView.swift
+++ Day3/GestureSample/GestureSample/TapGestureView.swift
@@ -10,7 +10,12 @@ import SwiftUI
 struct TapGestureView: View {
     var body: some View {
         VStack {
-            Text("Tap!")
+            Text("Tap Me!")
+                .font(.largeTitle)
+                .foregroundStyle(.blue)
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color.yellow)
         }
         .navigationTitle("Tap Gesture")
         .navigationBarTitleDisplayMode(.inline)
