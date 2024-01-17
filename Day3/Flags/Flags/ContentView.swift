6f920a782e5f2d64818113fe0542084c73ef4a37
Kiyong Kim <scgyong@gmail.com>
2024-01-17 10:56:41 +0900

    Argentina 국기 표시

--- Day3/Flags/Flags/ContentView.swift
+++ Day3/Flags/Flags/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image("argentina")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            Text("Argentina")
         }
         .padding()
     }
