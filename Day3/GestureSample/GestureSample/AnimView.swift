4d3ff6c3c5b5d4ec85ddd40418d1c29c6488e19f
Kiyong Kim <scgyong@gmail.com>
2024-01-18 12:38:13 +0900

    등장하자마자 애니메이션이 시작되게 한다

--- Day3/GestureSample/GestureSample/AnimView.swift
+++ Day3/GestureSample/GestureSample/AnimView.swift
@@ -8,24 +8,31 @@
 import SwiftUI
 
 struct AnimView: View {
-    @State var spinning = false
+    @State var spinning = true
+    @State var angle = Angle.zero
     var body: some View {
         VStack {
             Image(systemName: "arrow.clockwise.circle")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .foregroundStyle(.purple)
-                .rotationEffect(.degrees(spinning ? 90 : 0))
+                .rotationEffect(angle)
                 .animation(
-                    .linear(duration: 1.0),
-                    value: spinning ? 90 : 0
+                    .linear(duration: 1.0)
+                    .repeatForever(autoreverses: false),
+                    value: angle
                 )
                 .padding()
+                // animation vs transition
+            
             Toggle(isOn: $spinning, label: {
                 Text("Spins")
                     .font(.title)
             })
             .frame(width: 200)
+        }
+        .onAppear {
+            angle = .degrees(spinning ? 360 : 0)
         }
         .navigationTitle("Animation")
         .navigationBarTitleDisplayMode(.inline)
