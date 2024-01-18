67ad6f95beb8404f9888ab2a4c45be1fca5690d5
Kiyong Kim <scgyong@gmail.com>
2024-01-18 13:48:22 +0900

    MemoryGame Skeleton

---
+++ Day4/MemoryGame/MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Kiyong Kim on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
