ebb5a6be0872619039930f61115d187597d94a33
Kiyong Kim <scgyong@gmail.com>
2024-01-18 11:14:06 +0900

    여러 Shapre 들 추가

--- Day3/GestureSample/GestureSample/ShapeView.swift
+++ Day3/GestureSample/GestureSample/ShapeView.swift
@@ -10,7 +10,43 @@ import SwiftUI
 struct ShapeView: View {
     var body: some View {
         VStack {
-            Text("Hello, World!")
+            Color.red
+                .frame(width: 200, height: 50)
+            Rectangle()
+                .stroke(lineWidth: 8)
+                .fill(
+                    LinearGradient(
+                        colors: [ .red, .blue ],
+                        startPoint: .topLeading,
+                        endPoint: .bottomTrailing
+                    )
+                )
+                .frame(height: 50)
+                .cornerRadius(10.0)
+                .padding()
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 8)
+                .frame(height: 50)
+                .padding(.horizontal)
+            Capsule()
+                .stroke(lineWidth: 10)
+                .frame(height: 50)
+                .padding(.horizontal)
+                .padding(.top)
+            Circle()
+                .frame(width: 100, height: 50)
+            Ellipse()
+                .frame(width: 100, height: 50)
+                .padding()
+            Path { g in // Graphics
+                g.move(to: .zero)
+                g.addLine(to: CGPoint(x: 100, y: 200))
+                g.addLine(to: CGPoint(x: 200, y: 100))
+                g.addLine(to: CGPoint(x: 150, y: 50))
+                g.closeSubpath()
+            }
+            .stroke(lineWidth: 5)
+            .fill(.blue)
         }
         .navigationTitle("Drawings")
         .navigationBarTitleDisplayMode(.inline)
