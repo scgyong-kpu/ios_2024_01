fd7ec0f19702c90f83e6e39e4bd678d4232ec936
Kiyong Kim <scgyong@gmail.com>
2024-01-19 11:00:50 +0900

    MenuItem 꾸미기 2차

--- Day4/MemoryGame/MemoryGame/MenuView.swift
+++ Day4/MemoryGame/MemoryGame/MenuView.swift
@@ -16,6 +16,13 @@ struct MenuView: View {
                     MenuItemView(prefix: prefix)
                 }
             }
+            .background(
+                LinearGradient(
+                    colors: [.white, .orange.opacity(0.5) ],
+                    startPoint: .topLeading,
+                    endPoint: .bottomTrailing
+                )
+            )
             .navigationTitle("Memory Game")
         }
     }
@@ -29,27 +36,36 @@ struct MenuItemView: View {
     let prefix: String
     var body: some View {
         HStack {
+            Spacer()
             Image("\(prefix)_back")
             Spacer()
             NavigationLink {
                 GameView(prefix: prefix)
             } label: {
-                Text("Game \(prefix)")
+                Text("Start Game")
+                    .padding()
+                    .background(
+                        Capsule()
+                            .stroke(lineWidth: 5)
+                    )
             }
+            Spacer()
         }
         .frame(height: 300)
         .background(
             ZStack {
-//                Color.blue
-//                    .clipShape(RoundedRectangle(cornerRadius: 30))
-//                    .padding(20)
                 RoundedRectangle(cornerRadius: 30)
-                    .fill(Color.blue)
+                    .fill(Color.white)
                     .padding(20)
                 Image("\(prefix)_bg")
                     .resizable()
                 .padding(40)
             }
+            .rotation3DEffect(
+                .degrees(30),
+                axis: (x: 0.0, y: -1.0, z: 0.0)
+            )
+            .opacity(0.3)
         )
     }
 }
