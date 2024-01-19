fce3e89931748a41331053a1acab6ae861d70d3a
Kiyong Kim <scgyong@gmail.com>
2024-01-19 13:39:50 +0900

    PoiDataStore 를 Singleton 으로 만들고 items 를 Published 로 서언

--- Day5/PoiSample/PoiSample/PoiDataStore.swift
+++ Day5/PoiSample/PoiSample/PoiDataStore.swift
@@ -7,9 +7,18 @@
 
 import Foundation
 
+typealias PoiItem = Row
+
 class PoiDataStore: ObservableObject {
     struct Const {
         static let baseUrl = "https://openapi.gg.go.kr/Genrestrtfastfood"
         static let key = "e92ec3e6a9f44981974d4746d28f8995"
     }
+    @Published var items = [PoiItem]()
+
+    static let shared = PoiDataStore()
+    private init() {}
 }
+
+
+
