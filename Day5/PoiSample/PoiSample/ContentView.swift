8f808d15c8e1f321fe2fb9d6af24e6ccba581d99
Kiyong Kim <scgyong@gmail.com>
2024-01-19 13:15:10 +0900

    PoiSample Skeleton

---
+++ Day5/PoiSample/PoiSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PoiSample
+//
+//  Created by Kiyong Kim on 1/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day5/PoiSample/PoiSample/PoiSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoiSampleApp.swift
+//  PoiSample
+//
+//  Created by Kiyong Kim on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct PoiSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
